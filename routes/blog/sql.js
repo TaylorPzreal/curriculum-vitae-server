4f05eb6f6bf23713f5bab39b99fa241655973ef4
taylorpzreal <taylorpzreal@taylorpaldeiMac.lan>
2017-07-19 07:33:27 +0800

    添加新的blog查询sql

--- routes/blog/sql.js
+++ routes/blog/sql.js
@@ -4,7 +4,9 @@ const sql = {
   update: 'UPDATE blog set title=?, detail=? where titleId=?',
   delete: 'DELETE FROM blog WHERE titleId=?',
   queryByTitleId: 'SELECT * FROM blog WHERE titleId=?',
-  queryAll: 'SELECT * FROM blog'
+  queryAll: 'SELECT * FROM blog',
+  queryByPage: 'SELECT id, title, author, visites FROM blog LIMIT 10 offset ?',
+  queryTop10Hot: 'select id, title, author, visites from blog order by visites desc LIMIT 10'
 };
 
 module.exports = sql;
