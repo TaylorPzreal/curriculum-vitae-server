0ae345ef6afdadadb8f474e1510c0c9195b80a79
taylorpzreal <taylorpzreal@gmail.com>
2017-07-21 18:20:45 +0800

    update sql

--- routes/account/sql.js
+++ routes/account/sql.js
@@ -1,5 +1,5 @@
 const sql = {
-  'saveUser': 'INSERT INTO user(id,name,email,location,age,company,logo,bio,github_website,blog_website, followers, following) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)',
+  'saveUser': 'INSERT INTO user(uid,name,email,location,age,company,logo,bio,github_website,blog_website, followers, following) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)',
   'queryUserById': 'select * from user where id=?',
   'updateUserById': 'UPDATE user set name=?, email=?, location=?, age=?,company=?,logo=?,bio=?, followers=?, following=? where id=?'
 };
