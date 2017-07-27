6540ea82398bca3683454b7cce8c7a200a4c5493
taylorpzreal <taylorpzreal@gmail.com>
2017-07-27 13:25:54 +0800

    update upload image

--- routes/upload/index.js
+++ routes/upload/index.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
-const upload = multer({dest: 'uoloads/'});
 
-router.post('/image', upload.single('image'), (req, res, next) => {
-  console.warn(req.body);
-  console.warn(req.query);
-  console.warn(req.params);
-  console.warn('upload image success');
-  res.json('Upload Image Success');
+const uploadPath = '../curriculum-vitae/upload/images/';
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination (req, file, cb) {
+    cb(null, uploadPath);
+  },
+  filename (req, file, cb) {
+    const post = file.originalname.match(/\.(png|jpeg|gif|bmp|tiff|svg|jpg)$/g)[0];
+    cb(null, `${file.fieldname}-${Date.now()}${post}`);
+  }
+});
+const upload = multer({
+  dest: uploadPath,
+  limits:  {
+    fileSize: 512000
+  },
+  storage
+});
+
+// 上传图片
+router.post('/image', upload.single('image'), (req, res) => {
+  res.json({
+    code: 2000,
+    data: req.file.path.replace(/..\/curriculum-vitae/, ''),
+    msg: 'Success'
+  });
 });
 
 module.exports = router;
