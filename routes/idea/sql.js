2f647fcda3e5eb70227c329b49bb4ae361743b59
taylorpzreal <taylorpzreal@taylorpaldeiMac.lan>
2017-08-06 02:58:41 +0800

    update desc query

--- routes/idea/sql.js
+++ routes/idea/sql.js
@@ -1,6 +1,6 @@
 const sql = {
   insert: 'insert into idea(detail, uname, ulogo, uid) value(?, ?, ?, ?)',
-  queryMore: 'select * from idea limit 15 offset ?',
+  queryMore: 'select * from idea order by ctime desc limit 15 offset ?',
   changeCollection: 'update idea set collection=? where id=?'
 };
 module.exports = sql;
