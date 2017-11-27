147e25028b593199472aa1ce9e2459e59b166f36
TaylorPzreal <taylorpzreal@gmail.com>
2017-11-27 18:18:47 +0800

    fix: error param

--- app.js
+++ app.js
@@ -121,8 +121,7 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res) => {
-
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.msg;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
