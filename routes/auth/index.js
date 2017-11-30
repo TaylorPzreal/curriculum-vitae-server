3d6f8de3c9d5bf8ba5c5ebd8fe2b22cd05de835b
TaylorPzreal <taylorpzreal@gmail.com>
2017-11-30 19:09:07 +0800

    config geetest

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -2,10 +2,69 @@ const express = require('express');
 const router = express.Router();
 
 const passport = require('../../config/passport');
+const AccountModel = require('../../models/account.model');
+
 const Geetest = require('gt3-sdk');
 const geetestConfig = require('../../config/password').geetest;
 const captcha = new Geetest(geetestConfig);
-const AccountModel = require('../../models/account.model');
+
+// Geetest
+router.get('/gt/register', (req, res, next) => {
+  return captcha.register({
+    client_type: 'unknown',
+    ip_address: 'unknown'
+  }, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    
+    if (!data.success) {
+
+      // failback
+      req.session.fallback = true;
+      res.send(data);
+    } else {
+      req.session.fallback = false;
+
+      res.send({
+        code: 2000,
+        data,
+        msg: 'success'
+      });
+    }
+  });
+});
+
+router.post('/gt/validate', (req, res, next) => {
+  return captcha.validate(req.session.fallback, {
+
+    geetest_challenge: req.body.geetest_challenge,
+    geetest_validate: req.body.geetest_validate,
+    geetest_seccode: req.body.geetest_seccode
+
+  }, (err, success) => {
+    if (err) {
+      res.send({
+        code: 4000,
+        data: null,
+        msg: err
+      });
+    } else if (!success) {
+      res.send({
+        code: 9001,
+        data: null,
+        msg: 'Validate false'
+      });
+    } else {
+      res.send({
+        code: 2000,
+        data: null,
+        msg: 'Validate success'
+      });
+    }
+  });
+});
 
 router.post('/login', (req, res, next) => {
   passport.authenticate('local-login', (err, user, info, status) => {
